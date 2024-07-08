2f966bf184e8fae69c7c4a65a6222dd5b4772655
isismeira <isismeira127@gmail.com>
2024-07-08 13:20:12 -0300

    att

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const fs = require('fs')
+const trataErros = require('./erros/funcoesErro')
 
 const caminhoArquivo = process.argv
 const link = caminhoArquivo[2]
@@ -10,8 +11,7 @@ fs.readFile(link, 'utf-8', (erro, texto) => {
         if (erro) throw erro
         contaPalavras(texto)
     } catch (erro) {
-       if (erro.code === 'ENOENT') console.log('erro que esperava')
-       else console.log('outro erro') 
+       console.log(trataErros(erro))
     }
 }) 
 
